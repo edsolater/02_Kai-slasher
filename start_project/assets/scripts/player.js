7e151f7eba77cd0f948657153ecc805f2a089de3
zhanggongying <edsolater@qq.com>
2019-12-22 19:11:52 +0800

    init

--- start_project/assets/scripts/player.js
+++ start_project/assets/scripts/player.js
@@ -1,13 +1,3 @@
-// Learn cc.Class:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
-
 cc.Class({
   extends: cc.Component,
 
@@ -19,9 +9,11 @@ cc.Class({
     // 最大移动速度
     maxMoveSpeed: 0,
     // 加速度
-    accel: 0
+    accel: 0,
+
   },
-  setJumpAction: function() {
+
+  setJumpAction() {
     // 跳跃上升
     const jumpUp = cc
       .moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight))
@@ -33,6 +25,7 @@ cc.Class({
     // 不断重复
     return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
   },
+
   onKeyDown(event) {
     // set a flag when key pressed
     switch (event.keyCode) {
@@ -57,8 +50,42 @@ cc.Class({
     }
   },
 
+  update(dt) {
+    // 根据当前加速度方向每帧更新速度
+    if (this.accLeft) {
+      this.xSpeed -= this.accel * dt;
+    } else if (this.accRight) {
+      this.xSpeed += this.accel * dt;
+    }
+    // 限制主角的速度不能超过最大值
+    if (Math.abs(this.xSpeed) > this.maxMoveSpeed) {
+      // if speed reach limit, use max speed with current direction
+      this.xSpeed = this.maxMoveSpeed * Math.sign(this.xSpeed);
+    }
+
+    // 根据当前速度更新主角的位置
+    this.node.x += this.xSpeed * dt;
+  },
+
   onLoad() {
     // 初始化跳跃动作
-    this.node.runAction(this.setJumpAction());
+    this.jumpAction = this.setJumpAction();
+    this.node.runAction(this.jumpAction);
+
+    // 加速度方向开关
+    this.accLeft = false;
+    this.accRight = false;
+    // 主角当前水平方向速度
+    this.xSpeed = 0;
+
+    // 初始化键盘输入监听
+    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+  },
+
+  onDestroy() {
+    // 取消键盘输入监听
+    cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
   }
 });
