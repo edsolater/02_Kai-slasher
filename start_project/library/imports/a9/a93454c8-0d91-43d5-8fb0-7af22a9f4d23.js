8b46350a3b5cfd210f0dbd2ea301b905b4a4900c
zhanggongying <edsolater@qq.com>
2019-12-22 12:33:05 +0800

    init

---
+++ start_project/library/imports/a9/a93454c8-0d91-43d5-8fb0-7af22a9f4d23.js
@@ -0,0 +1,66 @@
+"use strict";
+cc._RF.push(module, 'a9345TIDZFD1Y+wevIqn00j', 'player');
+// scripts/player.js
+
+"use strict";
+
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+  extends: cc.Component,
+
+  properties: {
+    // 主角跳跃高度
+    jumpHeight: 0,
+    // 主角跳跃持续时间
+    jumpDuration: 0,
+    // 最大移动速度
+    maxMoveSpeed: 0,
+    // 加速度
+    accel: 0
+  },
+  setJumpAction: function setJumpAction() {
+    // 跳跃上升
+    var jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+    // 下落
+    var jumpDown = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+    // 不断重复
+    return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+  },
+  onKeyDown: function onKeyDown(event) {
+    // set a flag when key pressed
+    switch (event.keyCode) {
+      case cc.macro.KEY.a:
+        this.accLeft = true;
+        break;
+      case cc.macro.KEY.d:
+        this.accRight = true;
+        break;
+    }
+  },
+  onKeyUp: function onKeyUp(event) {
+    // unset a flag when key released
+    switch (event.keyCode) {
+      case cc.macro.KEY.a:
+        this.accLeft = false;
+        break;
+      case cc.macro.KEY.d:
+        this.accRight = false;
+        break;
+    }
+  },
+  onLoad: function onLoad() {
+    // 初始化跳跃动作
+    this.node.runAction(this.setJumpAction());
+  }
+});
+
+cc._RF.pop();
---
+++ start_project/temp/quick-scripts/assets/scripts/player.js
@@ -0,0 +1,78 @@
+(function() {"use strict";var __module = CC_EDITOR ? module : {exports:{}};var __filename = 'preview-scripts/assets/scripts/player.js';var __require = CC_EDITOR ? function (request) {return cc.require(request, require);} : function (request) {return cc.require(request, __filename);};function __define (exports, require, module) {"use strict";
+cc._RF.push(module, 'a9345TIDZFD1Y+wevIqn00j', 'player', __filename);
+// scripts/player.js
+
+"use strict";
+
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+  extends: cc.Component,
+
+  properties: {
+    // 主角跳跃高度
+    jumpHeight: 0,
+    // 主角跳跃持续时间
+    jumpDuration: 0,
+    // 最大移动速度
+    maxMoveSpeed: 0,
+    // 加速度
+    accel: 0
+  },
+  setJumpAction: function setJumpAction() {
+    // 跳跃上升
+    var jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+    // 下落
+    var jumpDown = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+    // 不断重复
+    return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+  },
+  onKeyDown: function onKeyDown(event) {
+    // set a flag when key pressed
+    switch (event.keyCode) {
+      case cc.macro.KEY.a:
+        this.accLeft = true;
+        break;
+      case cc.macro.KEY.d:
+        this.accRight = true;
+        break;
+    }
+  },
+  onKeyUp: function onKeyUp(event) {
+    // unset a flag when key released
+    switch (event.keyCode) {
+      case cc.macro.KEY.a:
+        this.accLeft = false;
+        break;
+      case cc.macro.KEY.d:
+        this.accRight = false;
+        break;
+    }
+  },
+  onLoad: function onLoad() {
+    // 初始化跳跃动作
+    this.node.runAction(this.setJumpAction());
+  }
+});
+
+cc._RF.pop();
+        }
+        if (CC_EDITOR) {
+            __define(__module.exports, __require, __module);
+        }
+        else {
+            cc.registerModuleFunc(__filename, function () {
+                __define(__module.exports, __require, __module);
+            });
+        }
+        })();
+        //# sourceMappingURL=player.js.map
+        
